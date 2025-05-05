7a5b643eea872242b5327b456a7376d8b531a4b7
princewillchidi <127307527+princewillchidi@users.noreply.github.com>
2025-05-05 14:44:36 +0100

    first commit

--- resources/views/admin/show_post.blade.php
+++ resources/views/admin/show_post.blade.php
@@ -73,7 +73,7 @@
                                         @if (Str::startsWith($post->image, 'http'))
                                             <img src="{{ $post->image }}" alt="Image" width="100">
                                         @else
-                                            <p>No image</p>
+                                            <p>No Image</p>
                                         @endif
                                     </td>
 
